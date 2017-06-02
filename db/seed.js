bd9315d4f2722ad7b992ae8922fbc0f5e43d02bd
Whitney <whitney.purdum@gmail.com>
2017-06-02 14:56:34 -0400

    update seed data

--- db/seed.js
+++ db/seed.js
@@ -65,21 +65,58 @@ var react = new Framework({
 
 var vue = new Framework({
   title: "Vue.js",
-  notable: "Virtual Document Object Model and JSX(an extension of JavaScript that looks like looks and acts like HTML)"
-  summary: "Use React to create fast web applications that you want to take to scale. React allows you to update and re-render individual components of your application without refreshing the entire page."
-  language: "JavaScript library",
-  languageSite: "https://facebook.github.io/react/",
-  docs: "https://facebook.github.io/react/docs/installation.html",
-  npm: "npm install -g create-react-app; create-react-app my-app; cd my-app; npm start",
-  cdn: "",
+  notable: "It prides itself on being a simplified and streamlined framework that is less opinionated and more developer-friendly than other JavaScript frameworks."
+  summary: "Use Vue.js to build interactive user interfaces."
+  language: "JavaScript framework",
+  languageSite: "https://vuejs.org/",
+  docs: "https://vuejs.org/v2/api/",
+  npm: "$ npm install vue",
+  cdn: "<script src='https://unpkg.com/vue'></script>",
   bower: "",
-  latest: "v15.5.4",
-  github: "https://github.com/facebook/react",
-  twitter: "https://twitter.com/reactjs",
-  assistance: "https://facebook.github.io/react/tutorial/tutorial.html",
-  maintainers: "Facebook, Instagram",
-  image: "https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png"
+  latest: "",
+  github: "github.com/vuejs/vue",
+  twitter: "https://twitter.com/vuejs",
+  assistance: "https://vuejs.org/v2/guide/",
+  maintainers: "",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/215px-Vue.png"
 });
+
+var vue = new Framework({
+  title: "Vue.js",
+  notable: "It prides itself on being a simplified and streamlined framework that is less opinionated and more developer-friendly than other JavaScript frameworks."
+  summary: "Use Vue.js to build interactive user interfaces."
+  language: "JavaScript framework",
+  languageSite: "https://vuejs.org/",
+  docs: "https://vuejs.org/v2/api/",
+  npm: "$ npm install vue",
+  cdn: "<script src='https://unpkg.com/vue'></script>",
+  bower: "",
+  latest: "2.3.3",
+  github: "github.com/vuejs/vue",
+  twitter: "https://twitter.com/vuejs",
+  assistance: "https://vuejs.org/v2/guide/",
+  maintainers: "Evan You",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/215px-Vue.png"
+});
+
+var backbone = new Framework({
+  title: "Backbone.js",
+  notable: "It prides itself on being a simplified and streamlined framework that is less opinionated and more developer-friendly than other JavaScript frameworks."
+  summary: "Use Vue.js to build interactive user interfaces."
+  language: "JavaScript framework",
+  languageSite: "https://vuejs.org/",
+  docs: "https://vuejs.org/v2/api/",
+  npm: "$ npm install vue",
+  cdn: "<script src='https://unpkg.com/vue'></script>",
+  bower: "",
+  latest: "2.3.3",
+  github: "github.com/vuejs/vue",
+  twitter: "https://twitter.com/vuejs",
+  assistance: "https://vuejs.org/v2/guide/",
+  maintainers: "Evan You",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/215px-Vue.png"
+});
+
 
 // var ends = [frontend, backend]
 // var frameworks = [angular]
