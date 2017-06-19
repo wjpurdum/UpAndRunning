6930230a7efc4492d285370b893863caf77468b6
Whitney <whitney.purdum@gmail.com>
2017-06-19 18:22:53 -0400

    update mongo listeners

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,9 @@ var mongoose = require("mongoose");
 
 if(process.env.NODE_ENV == "production"){
   mongoose.connect(process.env.MONGOLAB_URL);
-}else{
+} else {
   mongoose.connect('mongodb://localhost/UpAndRunning');
 }
-
 
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
