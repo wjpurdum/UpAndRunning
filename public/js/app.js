d6e5d269cad919076da1444e91791f75b785fbe4
Whitney <whitney.purdum@gmail.com>
2017-06-20 16:10:05 -0400

    comment out D3 directive and SVG div

--- public/js/app.js
+++ public/js/app.js
@@ -42,42 +42,42 @@ angular
     FrameworkShowControllerFunction
   ])
   // Get this working for gold:
-  .directive('donutChart', function(){
-    function link(scope, el){
-      var data = scope.data
-      var color = d3.scale.category10()
-      var el = el[0]
-      var width = el.clientWidth
-      var height = el.clientHeight
-      var min = Math.min(width, height)
-      var pie = d3.layout.pie()
-        .value(function(d) {return d.number;})(data);
-      var arc = d3.svg.arc()
-        .outerRadius(min/2 * 0.9)
-        .innerRadius(min/2 * 0.5)
-      var labelArc = d3.arc()
-        .outerRadius(min/2 * .1)
-        .innerRadius(min/2 * .5)
-      var svg = d3.select(el).append('svg')
-        .attr({width: width, height: height})
-        .append('g')
-          .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')')
-        svg.selectAll('path').data(pie(data.number))
-          .enter().append('path')
-            .style('stroke', 'white')
-            .attr('d', arc)
-            .attr('fill', function(d, i){return color(i)})
-            .append("text")
-              .attr("transform", function(d) {return "translate(" + labelArc.centroid(d)+ ")"; })
-              .text(function(d) {return d.data.name;})
-              .style("fill", "#fff");
-    }
-    return {
-      link: link,
-      restrict: 'E',
-      scope: {data: '='}
-    }
-  })
+  // .directive('donutChart', function(){
+  //   function link(scope, el){
+  //     var data = scope.data
+  //     var color = d3.scale.category10()
+  //     var el = el[0]
+  //     var width = el.clientWidth
+  //     var height = el.clientHeight
+  //     var min = Math.min(width, height)
+  //     var pie = d3.layout.pie()
+  //       .value(function(d) {return d.number;})(data);
+  //     var arc = d3.svg.arc()
+  //       .outerRadius(min/2 * 0.9)
+  //       .innerRadius(min/2 * 0.5)
+  //     var labelArc = d3.arc()
+  //       .outerRadius(min/2 * .1)
+  //       .innerRadius(min/2 * .5)
+  //     var svg = d3.select(el).append('svg')
+  //       .attr({width: width, height: height})
+  //       .append('g')
+  //         .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')')
+  //       svg.selectAll('path').data(pie(data.number))
+  //         .enter().append('path')
+  //           .style('stroke', 'white')
+  //           .attr('d', arc)
+  //           .attr('fill', function(d, i){return color(i)})
+  //           .append("text")
+  //             .attr("transform", function(d) {return "translate(" + labelArc.centroid(d)+ ")"; })
+  //             .text(function(d) {return d.data.name;})
+  //             .style("fill", "#fff");
+  //   }
+  //   return {
+  //     link: link,
+  //     restrict: 'E',
+  //     scope: {data: '='}
+  //   }
+  // })
 
 
 
